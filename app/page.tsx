627baa5c269ea7d2c4551749a366784f834a3c4b
joe-boadi <joeboadi1@gmail.com>
2024-06-18 20:27:59 +0000

    Modify page with div tag and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Main from "@/app/Components/Main";
+import "@/app/Styles/styles.module.css";
 
 export default function Home() {
   return (
     <>
+    <div className='flex p-4 justify-center items-center'>
       <Main />
+    </div>
     </>
   );
 }
